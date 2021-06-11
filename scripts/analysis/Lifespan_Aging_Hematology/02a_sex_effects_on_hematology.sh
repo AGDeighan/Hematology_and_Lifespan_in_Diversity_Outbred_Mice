00466c5ca6fe2d0c53bf8bea69c1c270707636fb
AGDeighan <andrew.g.deighan@gmail.com>
2021-06-11 13:42:26 -0400

    Shell scripts for running aging, lifespan, and hematology analysis on the cluster

---
+++ scripts/analysis/Lifespan_Aging_Hematology/02a_sex_effects_on_hematology.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p compute -q batch
+#SBATCH -N 1 # number of nodes
+#SBATCH -n 64 # number of cores
+#SBATCH --mem=128GB # total memory
+#SBATCH -t 72:00:00 # time (D-HH:MM)
+#SBATCH -o slurm.%j.out # STDOUT
+#SBATCH -e slurm.%j.err # STDERR
+
+module load singularity
+singularity exec do_hema_ls.sif Rscript 02a_sex_effects_on_hematology.R
